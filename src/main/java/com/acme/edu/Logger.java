7f38a15dbd8edee271057be57359c7794c552073
Ekaterina Kuzina <ekaterina.kuzina2@yandex.ru>
2021-08-22 18:31:41 +0300

    Code reorganised. Byte logging test modified.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -4,13 +4,13 @@ import java.util.Objects;
 import static com.acme.edu.Logger.Classes.*;
 import static java.lang.Math.abs;
 
-public class Logger{
+public class Logger {
     public static final String PRIMITIVE_PREFIX = "primitive: ";
     public static final String CHAR_PREFIX = "char: ";
     public static final String STRING_PREFIX = "string: ";
     public static final String OBJECT_PREFIX = "reference: ";
 
-    enum Classes{
+    enum Classes {
         UNDEFINED,
         BOOLEAN, CHAR, BYTE, INTEGER,
         STRING, OBJECT
@@ -18,129 +18,16 @@ public class Logger{
 
     private static Classes lastLogClass = UNDEFINED;
 
+    private static byte byteBuffer;
+    private static long byteOverflowCounter;
+
     private static int intBuffer;
     private static long intOverflowCounter;
 
     private static String strBuffer;
     private static int strCounter;
 
-    private static byte byteBuffer;
-    private static long byteOverflowCounter;
-
-    public static void log(int message) {
-        flushOtherClass(INTEGER);
-        processIntOverflow(message);
-        setLastLogClass(INTEGER);
-    }
-
-    private static  void processIntOverflow(int message) {
-        if (message > 0) {
-            processIntMaxOverflow(message);
-        } else {
-            processIntMinOverflow(message);
-        }
-    }
-
-    private static void processIntMaxOverflow(int message /*>=0*/) {
-        long diff = Integer.MAX_VALUE-(long)intBuffer;
-        if ( diff > message) {
-            intBuffer += message;
-        } else { // only if intBuffer > 0
-            intBuffer = (int)(message-diff);
-            if (intOverflowCounter < 0) {
-                --intBuffer;
-            }
-            ++intOverflowCounter;
-        }
-    }
-
-    private static void processIntMinOverflow(int message) {
-        long diff = (long)intBuffer-Integer.MIN_VALUE;
-        if ( diff > abs(message)) {
-            intBuffer += message;
-        } else { // only if intBuffer < 0
-            intBuffer = (int)(message+diff);
-            if (intOverflowCounter > 0) {
-                --intBuffer;
-            }
-            --intOverflowCounter;
-        }
-    }
-
-    private static void flushIntBuffer() {
-            writeInteger();
-            cleanIntBuffer();
-    }
-
-    private  static void writeInteger() {
-        writeIntOverflowValue();
-        writeIntResidual();
-    }
-
-    private static void writeIntOverflowValue() {
-        int overflowValue = intOverflowCounter > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        for (int i = 0; i < abs(intOverflowCounter); i++) {
-            write(formatMessage(PRIMITIVE_PREFIX, overflowValue));
-        }
-    }
-
-    private static void writeIntResidual() {
-        // to avoid printing "MAX_VALUE\n0" or "MIN_VALUE\n0"
-        // but allow "0" (when "MAX_VALUE" or "MIN_VALUE" is not needed)
-        if (intBuffer != 0 || intOverflowCounter == 0) {
-            write(formatMessage(PRIMITIVE_PREFIX, intBuffer));
-        }
-    }
-
-    private static void cleanIntBuffer() {
-        intBuffer = 0;
-        intOverflowCounter = 0;
-    }
-
-    //--------------------------------------------
-    public static void log(String message) {
-        flushOtherClass(STRING);
-        processString(message);
-        setLastLogClass(STRING);
-    }
-
-    private static void processString(String message) {
-        if (lastLogClass != STRING) {
-            initStrBuffer(message);
-        } else {
-            processStrBuffer(message);
-        }
-    }
-
-    private static void initStrBuffer(String message) {
-        strBuffer = message;
-        ++strCounter;
-    }
-
-    private static void processStrBuffer(String message) {
-        if (Objects.equals(strBuffer, message)) {
-            ++strCounter;
-        } else {
-            flushStrBuffer();
-            initStrBuffer(message);
-        }
-    }
-    private static void flushStrBuffer() {
-            write(formatMessage(STRING_PREFIX, formatString(strBuffer, strCounter)));
-            cleanStrBuffer();
-    }
-
-    private static String formatString(String strBuffer, int strCounter) {
-        if (strCounter == 1) return strBuffer;
-        return strBuffer + " (x" + strCounter + ")";
-    }
-
-    private static void cleanStrBuffer() {
-        strBuffer = null;
-        strCounter = 0;
-    }
-
-    //--------------------------------------------
+    //Byte logging
     public static void log(byte message) {
         flushOtherClass(BYTE);
         processByteOverflow(message);
@@ -211,7 +98,121 @@ public class Logger{
         byteOverflowCounter = 0;
     }
 
-    //--------------------------------------------
+    //Integer logging
+    public static void log(int message) {
+        flushOtherClass(INTEGER);
+        processIntOverflow(message);
+        setLastLogClass(INTEGER);
+    }
+
+    private static  void processIntOverflow(int message) {
+        if (message > 0) {
+            processIntMaxOverflow(message);
+        } else {
+            processIntMinOverflow(message);
+        }
+    }
+
+    private static void processIntMaxOverflow(int message /*>=0*/) {
+        long diff = Integer.MAX_VALUE-(long)intBuffer;
+        if ( diff > message) {
+            intBuffer += message;
+        } else { // only if intBuffer > 0
+            intBuffer = (int)(message-diff);
+            if (intOverflowCounter < 0) {
+                --intBuffer;
+            }
+            ++intOverflowCounter;
+        }
+    }
+
+    private static void processIntMinOverflow(int message) {
+        long diff = (long)intBuffer-Integer.MIN_VALUE;
+        if ( diff > abs(message)) {
+            intBuffer += message;
+        } else { // only if intBuffer < 0
+            intBuffer = (int)(message+diff);
+            if (intOverflowCounter > 0) {
+                --intBuffer;
+            }
+            --intOverflowCounter;
+        }
+    }
+
+    private static void flushIntBuffer() {
+            writeInteger();
+            cleanIntBuffer();
+    }
+
+    private  static void writeInteger() {
+        writeIntOverflowValue();
+        writeIntResidual();
+    }
+
+    private static void writeIntOverflowValue() {
+        int overflowValue = intOverflowCounter > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        for (int i = 0; i < abs(intOverflowCounter); i++) {
+            write(formatMessage(PRIMITIVE_PREFIX, overflowValue));
+        }
+    }
+
+    private static void writeIntResidual() {
+        // to avoid printing "MAX_VALUE\n0" or "MIN_VALUE\n0"
+        // but allow "0" (when "MAX_VALUE" or "MIN_VALUE" is not needed)
+        if (intBuffer != 0 || intOverflowCounter == 0) {
+            write(formatMessage(PRIMITIVE_PREFIX, intBuffer));
+        }
+    }
+
+    private static void cleanIntBuffer() {
+        intBuffer = 0;
+        intOverflowCounter = 0;
+    }
+
+    //String logging
+    public static void log(String message) {
+        flushOtherClass(STRING);
+        processString(message);
+        setLastLogClass(STRING);
+    }
+
+    private static void processString(String message) {
+        if (lastLogClass != STRING) {
+            initStrBuffer(message);
+        } else {
+            processStrBuffer(message);
+        }
+    }
+
+    private static void initStrBuffer(String message) {
+        strBuffer = message;
+        ++strCounter;
+    }
+
+    private static void processStrBuffer(String message) {
+        if (Objects.equals(strBuffer, message)) {
+            ++strCounter;
+        } else {
+            flushStrBuffer();
+            initStrBuffer(message);
+        }
+    }
+    private static void flushStrBuffer() {
+            write(formatMessage(STRING_PREFIX, formatString(strBuffer, strCounter)));
+            cleanStrBuffer();
+    }
+
+    private static String formatString(String strBuffer, int strCounter) {
+        if (strCounter == 1) return strBuffer;
+        return strBuffer + " (x" + strCounter + ")";
+    }
+
+    private static void cleanStrBuffer() {
+        strBuffer = null;
+        strCounter = 0;
+    }
+
+    //Other types and classes logging
     public static void log(Object message) {
         flushOtherClass(OBJECT);
         write(formatMessage(OBJECT_PREFIX, message));
@@ -230,7 +231,7 @@ public class Logger{
         setLastLogClass(BOOLEAN);
     }
 
-    //--------------------------------------------
+    //Service methods
     private static String formatMessage(String prefix, Object message) {
         return prefix + message;
     }
